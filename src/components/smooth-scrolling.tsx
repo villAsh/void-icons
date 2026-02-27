8d4204d702124de35306fc4e37c1ff5717f23610
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-02-27 08:44:19 +0530

    feat: added lenis and created smooth scroll effect

---
+++ src/components/smooth-scrolling.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export function SmoothScrolling({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.08, // Awwwards style smooth scrolling
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
