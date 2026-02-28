878787d83077a22ceb2f2f184287e35960b9c264
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-02-28 22:21:01 +0530

    feat: created features section

---
+++ src/icons/zap.tsx
@@ -0,0 +1,57 @@
+import { cn } from "@/lib/utils";
+import type { IconProps } from "@/types/icon";
+import { motion, type Variants } from "motion/react";
+
+const pathVariants: Variants = {
+  initial: { pathLength: 0, opacity: 0 },
+  animate: {
+    pathLength: 1,
+    opacity: 1,
+    transition: { duration: 0.5, ease: "easeInOut" },
+  },
+  hover: {
+    pathLength: [0, 1],
+    opacity: [1, 0.6, 1, 0.7, 1],
+    x: [0, -1, 1, -1, 0],
+    transition: {
+      duration: 0.4,
+      ease: "easeOut",
+    },
+  },
+};
+
+const Zap = ({
+  size = 24,
+  isActive,
+  className,
+  strokeWidth = 1.5,
+  width,
+  height,
+  ...rest
+}: IconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      initial="initial"
+      animate={isActive ? "hover" : "animate"}
+      whileTap="tap"
+      {...rest}
+      width={size ?? width ?? 24}
+      height={size ?? height ?? 24}
+      strokeWidth={strokeWidth}
+      className={cn("lucide lucide-zap", className)}
+    >
+      <motion.path
+        variants={pathVariants}
+        d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"
+      />
+    </motion.svg>
+  );
+};
+
+export default Zap;
