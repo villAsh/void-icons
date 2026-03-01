0f3f4fa00cef605e13242134b63cd73cb66c2810
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-03-01 11:03:40 +0530

    icon: created heart icon

---
+++ src/icons/heart.tsx
@@ -0,0 +1,57 @@
+import { cn } from "@/lib/utils";
+import type { IconProps } from "@/types/icon";
+import { motion, type Variants } from "motion/react";
+
+const pathVariants: Variants = {
+  initial: { pathLength: 1, opacity: 1 },
+  animate: {
+    scale: 1,
+    pathLength: 1,
+    opacity: 1,
+    transition: { duration: 0.8, ease: "easeInOut" },
+  },
+  hover: {
+    scale: [1, 1.1, 0.9, 1.1, 1],
+    opacity: 1,
+    transition: {
+      duration: 0.82,
+      ease: "easeInOut",
+      times: [0, 0.2, 0.4, 0.55, 0.7, 0.85, 1],
+    },
+  },
+};
+
+const Heart = ({
+  size = 24,
+  className,
+  strokeWidth = 2,
+  width,
+  height,
+  ...rest
+}: IconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      initial="initial"
+      animate={"animate"}
+      whileHover={"hover"}
+      {...rest}
+      width={size ?? width ?? 24}
+      height={size ?? height ?? 24}
+      strokeWidth={strokeWidth}
+      className={cn("lucide lucide-heart", className)}
+    >
+      <motion.path
+        variants={pathVariants}
+        d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"
+      />
+    </motion.svg>
+  );
+};
+
+export default Heart;
