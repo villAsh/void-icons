7e02f2adc9a8c3bd2fa5f4efad665d423df83bbe
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-03-04 19:14:15 +0530

    icon: created check icon

---
+++ src/icons/check.tsx
@@ -0,0 +1,53 @@
+import { cn } from "@/lib/utils";
+import type { IconProps } from "@/types/icon";
+import { motion, type Variants } from "motion/react";
+
+const pathVariants: Variants = {
+  initial: {
+    pathLength: 1,
+    scale: 1,
+    x: 0,
+    y: 0,
+  },
+  hover: {
+    scale: [1, 1.15, 1],
+    y: [2, 1, 2, 4, 2, 1],
+    pathLength: [1, 0.01, 1],
+    transition: {
+      duration: 0.48,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const Check = ({
+  size = 24,
+  className,
+  strokeWidth = 2,
+  width,
+  height,
+  ...rest
+}: IconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...rest}
+      width={size ?? width ?? 24}
+      height={size ?? height ?? 24}
+      strokeWidth={strokeWidth}
+      initial="hidden"
+      animate="initial"
+      whileHover="hover"
+      className={cn("lucide lucide-check-icon lucide-check", className)}
+    >
+      <motion.path variants={pathVariants} d="M20 6 9 17l-5-5" />
+    </motion.svg>
+  );
+};
+
+export default Check;
