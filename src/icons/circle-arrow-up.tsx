6fca8415e2f7576b71700fe2b0a6d5a9adc5277b
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-03-07 21:38:07 +0530

    icon: created circle-arrow-up icon

---
+++ src/icons/circle-arrow-up.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/lib/utils";
+import { motion, type SVGMotionProps, type Variants } from "motion/react";
+
+export interface IconProps extends SVGMotionProps<SVGSVGElement> {
+  size?: number;
+}
+
+const arrowVariants: Variants = {
+  initial: { y: 0 },
+  hover: {
+    y: -2,
+    transition: {
+      duration: 0.3,
+      ease: "backOut",
+    },
+  },
+};
+
+const CircleArrowUp = ({
+  size = 24,
+  className,
+  strokeWidth = 2,
+  width,
+  height,
+  ...rest
+}: IconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("lucide lucide-circle-arrow-up", className)}
+      width={size ?? width ?? 24}
+      height={size ?? height ?? 24}
+      strokeWidth={strokeWidth}
+      initial="initial"
+      whileHover="hover"
+      {...rest}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <motion.g variants={arrowVariants}>
+        <path d="m16 12-4-4-4 4" />
+        <path d="M12 16V8" />
+      </motion.g>
+    </motion.svg>
+  );
+};
+
+export default CircleArrowUp;
