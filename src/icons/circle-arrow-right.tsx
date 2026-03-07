827a2e39ca536539fd9a4a89ff7b6919227e99f3
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-03-07 21:37:57 +0530

    icon: created circle-arrow-right icon

---
+++ src/icons/circle-arrow-right.tsx
@@ -0,0 +1,52 @@
+import { cn } from "@/lib/utils";
+import { motion, type SVGMotionProps, type Variants } from "motion/react";
+
+export interface IconProps extends SVGMotionProps<SVGSVGElement> {
+  size?: number;
+}
+
+const arrowVariants: Variants = {
+  initial: { x: 0 },
+  hover: {
+    x: 2,
+    transition: {
+      duration: 0.3,
+      ease: "backOut",
+    },
+  },
+};
+
+const CircleArrowRight = ({
+  size = 24,
+  className,
+  strokeWidth = 2,
+  width,
+  height,
+  ...rest
+}: IconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("lucide lucide-circle-arrow-right", className)}
+      width={size ?? width ?? 24}
+      height={size ?? height ?? 24}
+      strokeWidth={strokeWidth}
+      initial="initial"
+      whileHover="hover"
+      {...rest}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <motion.g variants={arrowVariants}>
+        <path d="m12 16 4-4-4-4" />
+        <path d="M8 12h8" />
+      </motion.g>
+    </motion.svg>
+  );
+};
+
+export default CircleArrowRight;
