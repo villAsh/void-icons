76e8dc912ce47d28b52ccad29855dc05fda42ac2
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-03-07 21:26:57 +0530

    icon: created chart-column icon

---
+++ src/icons/chart-column.tsx
@@ -0,0 +1,79 @@
+import { cn } from "@/lib/utils";
+import { motion, type SVGMotionProps, type Variants } from "motion/react";
+
+export interface IconProps extends SVGMotionProps<SVGSVGElement> {
+  size?: number;
+}
+
+const column1Variants: Variants = {
+  initial: { pathLength: 1, opacity: 1 },
+  hover: {
+    pathLength: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      delay: 0,
+    },
+  },
+};
+
+const column2Variants: Variants = {
+  initial: { pathLength: 1, opacity: 1 },
+  hover: {
+    pathLength: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      delay: 0.1,
+    },
+  },
+};
+
+const column3Variants: Variants = {
+  initial: { pathLength: 1, opacity: 1 },
+  hover: {
+    pathLength: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      delay: 0.2,
+    },
+  },
+};
+
+const ChartColumn = ({
+  size = 24,
+  className,
+  strokeWidth = 2,
+  width,
+  height,
+  ...rest
+}: IconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("lucide lucide-chart-column", className)}
+      width={size ?? width ?? 24}
+      height={size ?? height ?? 24}
+      strokeWidth={strokeWidth}
+      initial="initial"
+      whileHover="hover"
+      {...rest}
+    >
+      <path d="M3 3v16a2 2 0 0 0 2 2h16" />
+      <motion.path variants={column3Variants} d="M18 17V9" />
+      <motion.path variants={column2Variants} d="M13 17V5" />
+      <motion.path variants={column1Variants} d="M8 17v-3" />
+    </motion.svg>
+  );
+};
+
+export default ChartColumn;
