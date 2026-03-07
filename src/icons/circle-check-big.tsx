307c704bdcc178cfb3c180a9e6a3f29e9acf9cea
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-03-07 21:38:16 +0530

    icon: created circle-check-big icon

---
+++ src/icons/circle-check-big.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/lib/utils";
+import { motion, type SVGMotionProps, type Variants } from "motion/react";
+
+export interface IconProps extends SVGMotionProps<SVGSVGElement> {
+  size?: number;
+}
+
+const checkVariants: Variants = {
+  initial: { pathLength: 1, opacity: 1 },
+  hover: {
+    pathLength: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const CircleCheckBig = ({
+  size = 24,
+  className,
+  strokeWidth = 2,
+  width,
+  height,
+  ...rest
+}: IconProps) => {
+  return (
+    <motion.svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("lucide lucide-circle-check-big", className)}
+      width={size ?? width ?? 24}
+      height={size ?? height ?? 24}
+      strokeWidth={strokeWidth}
+      initial="initial"
+      whileHover="hover"
+      {...rest}
+    >
+      <path d="M21.801 10A10 10 0 1 1 17 3.335" />
+      <motion.path variants={checkVariants} d="m9 11 3 3L22 4" />
+    </motion.svg>
+  );
+};
+
+export default CircleCheckBig;
