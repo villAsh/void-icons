3df4e95a93b658e0c5f035007433d68fd2dc3270
Vilas Chauvhan <chavhanvilas02@gmail.com>
2026-03-09 21:55:06 +0530

    feat: made pwa and updated favicon

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {},
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
